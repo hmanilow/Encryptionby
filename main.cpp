459bf47dee1c73c707f240ae01f4efc643d7acf7
Khussein Reber <73395161+hmanilow@users.noreply.github.com>
2023-05-13 20:36:40 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian"); //локаль на русский язык
+
+    int iter = 0;
+    string ai = ""; //номер строки
+    string aj = ""; //номер столба
+    string cryptage = ""; //шифр
+    string cryptocode = ""; //расшифр
+    string decrypted_text = ""; //еще раз расшифр, так надо
+}
